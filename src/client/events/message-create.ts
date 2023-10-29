c26301445986f5b8474ed183c3e94b1f0a52b729
Sylux <sy-lux@hotmail.fr>
2023-10-29 17:48:39 +0100

    feat(events): add bot reactions (#2)

---
+++ src/client/events/message-create.ts
@@ -0,0 +1,41 @@
+import { Message } from 'discord.js';
+
+import { privateGuildId } from '../../../config.json';
+import { client } from '../client';
+import { getYousoro } from '../utils';
+
+export function handleMessageCreate(message: Message<boolean>) {
+  const content = message.content.toLowerCase();
+
+  if (message.inGuild() && message.guildId === privateGuildId) {
+    if (
+      (content.includes('yousoro') || content.includes('sylux6yo')) &&
+      !message.author.bot
+    ) {
+      const reaction = message.guild?.emojis?.cache?.find(
+        ({ name }) => name === 'yousoro',
+      );
+      if (reaction) {
+        message.react(reaction);
+      }
+    }
+  }
+
+  if (content.includes('zensoku zenshin')) {
+    message.channel.send('YOUSORO!~ (> ᴗ •)ゞ');
+  }
+
+  if (message.mentions.has(client.user!)) {
+    if (content.includes('lewd')) {
+      message.channel.send({
+        content: "I'm not lewd!",
+        reply: { messageReference: message },
+      });
+    } else {
+      message.channel.send({
+        content: getYousoro(message.guild),
+        reply: { messageReference: message },
+      });
+    }
+  }
+}
--- src/commands/fun/lewd.ts
+++ src/commands/fun/lewd.ts
@@ -2,7 +2,7 @@ import { SlashCommand } from '../../types/slash-command';
 
 export const lewdCommand = new SlashCommand()
   .setName('lewd')
-  .setDescription('I am not LEWD!')
+  .setDescription("I'm not LEWD!")
   .setExecute(interaction =>
     interaction.reply({ files: ['src/assets/notlewd.jpg'] }),
   );
