d6e25c4e727931adc0546541ba71e4947f43cb8e
Sylux <9683460+Sylux6@users.noreply.github.com>
2024-05-16 00:40:21 +0200

    feat(config): use environment variables if config.json is missing

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,10 +1,26 @@
+import * as fs from 'node:fs';
+
 import { PrismaClient } from '@prisma/client';
 import { Client, GatewayIntentBits, HexColorString, REST } from 'discord.js';
 
-import * as configJson from '../../config.json';
 import { Config } from '../types/config';
 
-export const config: Config = configJson;
+function getConfig(): Config {
+  if (fs.existsSync('config.json')) {
+    return JSON.parse(fs.readFileSync('config.json', 'utf-8'));
+  } else {
+    return {
+      token: process.env.TOKEN ?? '',
+      clientId: process.env.CLIENT_ID ?? '',
+      postgresDb: process.env.POSTGRES_DB ?? '',
+      postgresPassword: process.env.POSTGRES_PASSWORD ?? '',
+      postgresUser: process.env.POSTGRES_USER ?? '',
+      privateGuildId: process.env.PRIVATE_GUILD_ID,
+    };
+  }
+}
+
+export const config: Config = getConfig();
 export const client = new Client({
   intents: [
     GatewayIntentBits.DirectMessages,
