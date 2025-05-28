462f1abd0fe9eb85ca1391eb9128dd3ec294ddea
Sylux <9683460+Sylux6@users.noreply.github.com>
2025-05-28 17:17:25 +0200

    feat: better commands resilience on unavailable discord api

--- src/client/events/interaction-create.ts
+++ src/client/events/interaction-create.ts
@@ -10,41 +10,47 @@ export async function handleInteractionCreate(interaction: Interaction) {
     return;
   }
 
-  if (interaction.commandName in ALL_COMMANDS) {
-    try {
-      await ALL_COMMANDS[interaction.commandName].execute(interaction);
-    } catch (error) {
-      if (error instanceof NotImplementedError) {
-        interaction.reply({
-          embeds: [
-            createBotEmbed({
-              description: `Command ${interaction.commandName} is not implemented`,
-              replyType: ReplyType.ERROR,
-            }),
-          ],
-          ephemeral: true,
-        });
-      } else {
-        interaction.reply({
-          embeds: [
-            createBotEmbed({
-              description: `I couldn't execute your command:\n${error}`,
-              replyType: ReplyType.ERROR,
-            }),
-          ],
-          ephemeral: true,
-        });
+  try {
+    if (interaction.commandName in ALL_COMMANDS) {
+      try {
+        await ALL_COMMANDS[interaction.commandName].execute(interaction);
+      } catch (error) {
+        if (error instanceof NotImplementedError) {
+          interaction.reply({
+            embeds: [
+              createBotEmbed({
+                description: `Command ${interaction.commandName} is not implemented`,
+                replyType: ReplyType.ERROR,
+              }),
+            ],
+            ephemeral: true,
+          });
+        } else {
+          interaction.reply({
+            embeds: [
+              createBotEmbed({
+                description: `I couldn't execute your command:\n${error}`,
+                replyType: ReplyType.ERROR,
+              }),
+            ],
+            ephemeral: true,
+          });
+        }
       }
+    } else {
+      interaction.reply({
+        embeds: [
+          createBotEmbed({
+            description: `Command ${interaction.commandName} doesn't exist`,
+            replyType: ReplyType.ERROR,
+          }).setFooter({
+            text: 'request feature at https://github.com/Sylux6/WatanaBot.ts/issues',
+          }),
+        ],
+        ephemeral: true,
+      });
     }
-  } else {
-    interaction.reply({
-      embeds: [
-        createBotEmbed({
-          description: `Command ${interaction.commandName} doesn't exist`,
-          replyType: ReplyType.ERROR,
-        }).setFooter({ text: 'request feature at https://github.com/Sylux6/WatanaBot.ts/issues' }),
-      ],
-      ephemeral: true,
-    });
+  } catch (e) {
+    console.error(e);
   }
 }
